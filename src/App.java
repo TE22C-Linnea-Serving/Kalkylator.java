612251f04d36df772f786b098d21fbe868d55da9
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-11 08:59:27 +0200

    Beräkna produkt stora tal

---
+++ src/App.java
@@ -0,0 +1,36 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int tal1 = 23;
+        double tal2 = 45.0;
+        double summa;
+        double produkt; 
+
+        summa = tal1+tal2;
+        System.out.println(tal1+"+"+tal2+"="+summa);
+
+        summa = tal1-tal2;
+        System.out.println(tal1+"-"+tal2+"="+summa);
+
+        int tal3 = 234232;
+        int tal4 = 3424;
+
+        produkt = tal3*tal4;
+        System.out.println(tal3+"*"+tal4+"="+produkt);
+
+        produkt = tal2*tal3*tal4;
+        System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt);
+
+        
+
+
+
+
+
+
+
+
+
+    }
+}
